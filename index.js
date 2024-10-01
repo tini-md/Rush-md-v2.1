2b77a210ce2bf2725c474e38bc69d1683cf97398
queen-nithu <nimsaravihaga37@gmail.com>
2024-10-01 19:50:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const axios = require('axios')
 const { File } = require('megajs')
 const prefix = '.'
 
-const ownerNumber = ['94789123880']
+const ownerNumber = ['94762498519']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
