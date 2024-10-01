e17d5dcb989f601c2a0a665976cce95a6da0ef0d
queen-nithu <nimsaravihaga37@gmail.com>
2024-10-01 19:57:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "CKQ2DKIb#jxV1i28Yg2ff_yaqqkb7s1zFwwhz_NQlZQxZgczu_0s",
+SESSION_ID: process.env.SESSION_ID || "xyFGXAQb#EbmUcljACC2CIlX1qB0e4iT9_0N5PWmS3dyefrFoWF8",
 ALIVE_IMG : process.env.ALIVE_IMG || "https://telegra.ph/file/e1fd8689e69a7baa4920d.jpg",
 ALIVE_MSG : process.env.ALIVE_MSG || "hi",    
 };
