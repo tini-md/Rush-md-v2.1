750bc2a207e8e383ddf3074779e083eaf94c5ea5
tini-md <vihaganimsara045@gmail.com>
2024-10-03 18:26:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "Mi1jXZ5T#qGJpTDaeBh4ui1Y0s2K9C5Ri8ABjgN1wBqlmT-FGyEg",
 ALIVE_IMG : process.env.ALIVE_IMG || "https://telegra.ph/file/e1fd8689e69a7baa4920d.jpg",
 ALIVE_MSG : process.env.ALIVE_MSG || "HEY THEY i m rush",    
 };
